1d5dd67734e4ce8bbae35487289bea71157a3e60
PX-72 <zoli.konyves@pm.me>
2023-10-22 19:46:24 +0100

    minor change

--- src/api/marketDataApi.ts
+++ src/api/marketDataApi.ts
@@ -2,7 +2,7 @@ export interface MarketDataRequestConfiguration {
     configurationId: string;
     size: number;
     intervalInMillis: number;
-    intervalDelayInMillis: number;
+    initialDelayInMillis: number;
     ccyFilter: string[];
 }
 
--- src/components/ControlPanel/MarketDataRequest.tsx
+++ src/components/ControlPanel/MarketDataRequest.tsx
@@ -1,13 +1,5 @@
-import { sendRequest, MarketDataRequestConfiguration } from '@/api/marketDataApi.js';
+import { sendRequest } from '@/api/marketDataApi.js';
 import { DefaultButton } from '@/components/common/styles.js';
-
-const requestToSend: MarketDataRequestConfiguration = {
-    configurationId: '83Y4384Y',
-    size: 10,
-    intervalInMillis: 1000,
-    intervalDelayInMillis: 200,
-    ccyFilter: ['USDEUR', 'GBPUSD'], 
-};
 
 export const MarketDataRequest = () => {
 
@@ -16,7 +8,7 @@ export const MarketDataRequest = () => {
                 configurationId: '83Y4384Y',
                 size: 10,
                 intervalInMillis: 1000,
-                intervalDelayInMillis: 200,
+                initialDelayInMillis: 200,
                 ccyFilter: ['USDEUR', 'GBPUSD'], 
             })}
         >Send market data request</DefaultButton>
