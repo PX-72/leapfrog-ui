3633929dc209dc6d2a31893170d63f0630ab1b98
PX-72 <zoli.konyves@pm.me>
2023-12-03 20:44:00 +0000

    BLAH2

---
+++ src/api/comms/operationTypeEnum.ts
@@ -0,0 +1,5 @@
+
+export enum OperationTypeEnum {
+    ConnectToWebSocket = 0,
+    DisconnectFromWebSocket
+}
--- src/api/sharedWorker.ts
+++ src/api/sharedWorker.ts
@@ -1,15 +1,29 @@
+import { createWebSocket } from '@/api/comms/webSocketWrapper';
+import { OperationTypeEnum } from '@/api/comms/operationTypeEnum';
+import { MarketDataResponse } from '@/api/marketDataApi';
+
 export {};
 
 const ports: MessagePort[] = [];
-const sessionPortMap = new Map<string, MessagePort>();
-let isRunning = false;
-let counter = 0;
+const subscriptionPortsMap = new Map<string, MessagePort[]>();
 
 self.onconnect = ({ ports: [port] }: MessageEvent) => {
     ports.push(port);
 
     port.onmessage = (m: MessageEvent) => {
         console.log(`ID: ${m.data}`);
+
+
     };
 };
+
+const MARKET_DATA_URL = 'ws://localhost:8090/market-data-ws';
+
+const connectToWebSocket = () => {
+     createWebSocket(
+        MARKET_DATA_URL,
+        message => listener(deserialiseMarketDataMessage(message)),
+        () => {}
+    );
+}
 
