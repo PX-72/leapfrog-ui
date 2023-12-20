cf372672434f2b575f8138d189ae00dc586907e1
PX-72 <zoli.konyves@pm.me>
2023-12-20 11:13:36 +0000

    progress

--- src/api/comms/workerClient.ts
+++ src/api/comms/workerClient.ts
@@ -1,10 +1,36 @@
-export const start = () => {
-    // const id = crypto.randomUUID();
-    const worker = new SharedWorker('../sharedWorker.ts', { type: 'module' });
 
-    worker.onerror = ({ error }: ErrorEvent) => console.error(`Error in the shared worker: ${error}`);
-    worker.port.onmessageerror = ({ data }: MessageEvent) => console.error(`Error when communicating with the shared worker: ${data.toString()}`);
-    worker.port.onmessage = ({ data }: MessageEvent) => console.log(data.toString());
+export type WorkerClientInput = {
+    onMessage: ({ data }: MessageEvent) => void,
+    onError: (e: Error) => void
+};
 
-    worker.port.start();
+export type WorkerClientReturn = {
+    send: (message: string) => void,
+    close: () => void
+};
+
+
+const id = crypto.randomUUID();
+let worker: SharedWorker;
+
+const send = (message: string) => {
+    worker?.port.postMessage(message);
+};
+
+const close = () => {
+    worker?.port.close();
+}
+
+export const createWorkerClient = ({onMessage, onError}: WorkerClientInput): WorkerClientReturn => {
+
+    if (worker === undefined)
+        worker = new SharedWorker('../sharedWorker.ts', { type: 'module' });
+
+    worker.onerror = ({ error }: ErrorEvent) => onError(new Error(`Error in the shared worker: ${error}`));
+    worker.port.onmessageerror = ({ data }: MessageEvent) => onError(new Error(`Error when communicating with the shared worker: ${data.toString()}`));
+    worker.port.onmessage = onMessage;
+
+    //worker.port.start(); // not needed as worker.port.onmessage implicitly starts the port
+
+    return { send, close };
 };
